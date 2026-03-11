721a612bd7eb4d396d20d5cdd4e757744fb66918
MarcelleDev <devmarcelle94@gmail.com>
2026-03-11 09:46:08 -0300

    feat: atualizar consulta SQL para incluir média de preço e filtrar marcas com estoque baixo

--- 04_relatorios.sql
+++ 04_relatorios.sql
@@ -1,8 +1,10 @@
 SELECT 
-    marcas.nome_marca AS Marca,
-    COUNT(produtos.id_produto) AS Qtd_Modelos,
-    SUM(produtos.estoque) AS Total_Itens_Estoque,
-    SUM(produtos.preco * produtos.estoque) AS Valor_Total_Patrimonio
-FROM marcas
-LEFT JOIN produtos ON marcas.id_marca = produtos.id_marca
-GROUP BY marcas.nome_marca;
+    m.nome_marca AS Marca,
+    COUNT(p.id_produto) AS Qtd_Modelos,
+    SUM(p.estoque) AS Total_Estoque,
+    ROUND(AVG(p.preco), 2) AS Preco_Medio,
+    ROUND(SUM(p.preco * p.estoque), 2) AS Patrimonio_Total
+FROM marcas m
+LEFT JOIN produtos p ON m.id_marca = p.id_marca
+GROUP BY m.nome_marca
+HAVING SUM(p.estoque) < 10;
